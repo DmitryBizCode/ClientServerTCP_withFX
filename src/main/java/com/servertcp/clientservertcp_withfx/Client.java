dca790054317e2fd7b52a0a5e277886d6a6317a9
Dmitry Beznosiuk <145875719+DmitryBizCode@users.noreply.github.com>
2024-02-12 02:25:38 +0200

    client final set

--- src/main/java/com/servertcp/clientservertcp_withfx/Client.java
+++ src/main/java/com/servertcp/clientservertcp_withfx/Client.java
@@ -1,4 +1,71 @@
 package com.servertcp.clientservertcp_withfx;
 
-public class Client {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+public class Client extends Application {
+    @FXML
+    private TextField Xnum;
+
+    @FXML
+    private TextField Ynum;
+
+    @FXML
+    private TextField Cnum;
+
+    @FXML
+    private Label resultLabel;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ClientFX.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setTitle("Client!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+    @FXML
+    protected void calculate() {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("localhost", 12345);
+
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+                double value1 = Double.parseDouble(Xnum.getText());
+                double value2 = Double.parseDouble(Ynum.getText());
+                double value3 = Double.parseDouble(Cnum.getText());
+
+                dos.writeDouble(value1);
+                dos.writeDouble(value2);
+                dos.writeDouble(value3);
+
+                double result = dis.readDouble();
+
+                // Update the UI on the JavaFX Application Thread
+                Platform.runLater(() -> resultLabel.setText("Result: " + result));
+
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
--- src/main/java/com/servertcp/clientservertcp_withfx/Server.java
+++ src/main/java/com/servertcp/clientservertcp_withfx/Server.java
@@ -1,5 +1,73 @@
 package com.servertcp.clientservertcp_withfx;
 
-public class Server {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Application {
+    private Label resultLabel;
+
+    @Override
+    public void start(Stage primaryStage) {
+        resultLabel = new Label("Result will be displayed here");
+
+        VBox root = new VBox(resultLabel);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("TCP Server FX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        startServer();
+    }
+
+    private void startServer() {
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(12345);
+
+                while (true) {
+                    System.out.println("Waiting for client...");
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client connected.");
+
+                    DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                    DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+
+                    double value1 = dis.readDouble();
+                    double value2 = dis.readDouble();
+                    double value3 = dis.readDouble();
+
+                    double result = performCalculation(value1, value2, value3);
+
+                    // Відображення результату на UI потоці
+                    Platform.runLater(() -> resultLabel.setText("Result: " + result));
+
+                    dos.writeDouble(result);
+
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private double performCalculation(double value1, double value2, double value3) {
+        return value1 + value2 * value3;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
